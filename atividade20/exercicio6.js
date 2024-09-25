e1149c05c03676e9ee90001b99ef43dbf2b08a81
DanielFatec1911 <160775497+DanielFatec1911@users.noreply.github.com>
2024-09-24 22:04:49 -0300

    Delete atividade20 directory

--- atividade20/exercicio6.js
+++
@@ -1,8 +0,0 @@
-//criar servidor
-var http = require('http');
-var server = http.createServer(function (req,res){
-    res.end("<html><body>site da fatec Sorocaba</bodey></html>");
-
-});
-
-server.listen(3000);
